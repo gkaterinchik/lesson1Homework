b6763f801030de143b06ecbef15b79250545b657
gkaterinchik <62135154+gkaterinchik@users.noreply.github.com>
2020-11-30 10:07:42 +0300

    ДЗ № 2   задача 8

--- src/ru/geekbrains/lesson1/Replace.java
+++ src/ru/geekbrains/lesson1/Replace.java
@@ -2,35 +2,45 @@ package ru.geekbrains.lesson1;
 
 public class Replace {
 
-    static int[] arr = {1,2,3,4,5,6,7};
+    static int[] arr = {1,2,3,4,5,6,7,8};
 
     public static void main(String[] args) {
 
-        arrReplace(4);
 
+        replace(arr,-2);
     }
 
-
-    public static void arrReplace(int n)
+    public static int[] replace(int[] arr, int n)
     {
+        if (n==0) return arr;
+        if (n<0) n=arr.length+n;
+        for(int i=0;i<n; i++)
+        {
+            arrReplace();
+        }
+        for(int j=0; j<=arr.length-1; j++)
+            System.out.println(arr[j]);
+        return arr;
+    }
+     private static void arrReplace() {
         int a, b, c;
-        int pos=0;
-        a=arr[0];
-        for (int i = 0; i <= arr.length-1; i++) {
+        int pos = 0;
+        a = arr[0];
+        for (int i = 0; i <= arr.length - 1; i++) {
 
-            pos = pos+ n;
-            pos = pos%arr.length;
-           b=arr[pos] ;
-           arr[pos]=a;
-            a=b;
-
+            pos = pos + 1;
+            pos = pos % arr.length;
+            b = arr[pos];
+            arr[pos] = a;
+            a = b;
 
 
         }
-        for(int i=0; i<=arr.length-1; i++)
-            System.out.println(arr[i]);
-    }
 
+
+
+
+}
 
 }
 
